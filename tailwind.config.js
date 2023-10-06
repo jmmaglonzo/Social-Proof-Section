6b4b831092f56af69ee32e457532e764179db4b8
Lonzo <jmmaglonzo.dev@gmail.com>
2023-10-06 16:10:29 +0800

    complete project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Primary
+        veryDarkMagenta: "hsl(300, 43%, 22%)",
+        softPink: "hsl(333, 80%, 67%)",
+
+        // Neutral
+        darkGrayishMagenta: "hsl(303, 10%, 53%)",
+        lightGrayishMagenta: "hsl(300, 24%, 96%)",
+      },
+    },
+    container: {
+      center: true,
+    },
   },
   plugins: [],
 };
